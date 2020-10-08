ee35f49efe46a192855b4a2f2f45d4891ef54bda
Dominika <dominikaperkowska@gmail.com>
2020-10-08 19:54:39 -0400

    schema and seeds file added

--- config/orm.js
+++ config/orm.js
@@ -1,9 +1,11 @@
-* Import (require) `connection.js` into `orm.js`
+// * Import (require) `connection.js` into `orm.js`
 
-   * In the `orm.js` file, create the methods that will execute the necessary MySQL commands in the controllers. These are the methods you will need to use in order to retrieve and store data in your database.
+//    * In the `orm.js` file, create the methods that will execute the necessary MySQL commands in the controllers. These are the methods you will need to use in order to retrieve and store data in your database.
 
-     * `selectAll()`
-     * `insertOne()`
-     * `updateOne()`
+//      * `selectAll()`
+//      * `insertOne()`
+//      * `updateOne()`
 
-   * Export the ORM object in `module.exports`.
+//    * Export the ORM object in `module.exports`.
+
+
--- db/seeds.sql
+++ db/seeds.sql
@@ -1,1 +1,5 @@
 -- In this file, write insert queries to populate the `burgers` table with at least three entries.
+
+INSERT INTO burgers (name) VALUES ('Turkey Burger');
+INSERT INTO burgers (name) VALUES ('Double Cheese Burger');
+INSERT INTO burgers (name) VALUES ('Blue Cheese Burger');
