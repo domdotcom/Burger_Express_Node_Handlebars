d76c3d2a285455ed2a22c0942be66feb4dccb1a7
Dominika <dominikaperkowska@gmail.com>
2020-10-15 16:11:54 -0400

    fixed routes in server.js

--- models/burger.js
+++ models/burger.js
@@ -1,8 +1,8 @@
-* Inside `burger.js`, import `orm.js` into `burger.js`
+// * Inside `burger.js`, import `orm.js` into `burger.js`
 
-    * Also inside `burger.js`, create the code that will call the ORM functions using burger specific input for the ORM.
+//     * Also inside `burger.js`, create the code that will call the ORM functions using burger specific input for the ORM.
 
-    * Export at the end of the `burger.js` file.
+//     * Export at the end of the `burger.js` file.
 
     // Import the ORM to create functions that will interact with the database.
 const orm = require("../config/orm.js");
